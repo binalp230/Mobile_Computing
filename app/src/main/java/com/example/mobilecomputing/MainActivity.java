7d1bde2de45618d382b0c9108d0a21577b993850
hforhirenpatel <hforhirenpatel@gmail.com>
2022-01-21 21:32:05 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mobilecomputing/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mobilecomputing;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void on_click(View v){
+        TextView tv = findViewById(R.id.msg);
+        counter += 1;
+        tv.setText("counter: " + counter);
+    }
+}
